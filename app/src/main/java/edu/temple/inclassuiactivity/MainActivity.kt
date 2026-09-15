a8ee03c46251d855ebbf17efec306d71e53c7540
Karl Morris <karl.morris@temple.edu>
2026-09-15 13:33:25 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/inclassuiactivity/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.temple.inclassuiactivity
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val displayTextView = findViewById<TextView>(R.id.textDisplay)
+
+        /* Step 1: Populate this array */
+        val numberArray = Array(50){i -> (i + 1) * 2}
+
+        with (spinner) {
+            /* Step 2: Create adapter to display items from array in Spinner */
+            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
+
+
+            // Step 3: Change TextView's text size to the number selected in the Spinner */
+            onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    parent?.run {
+                        displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+            }
+        }
+
+    }
+}
